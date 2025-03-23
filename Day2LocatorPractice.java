81d1c80477d774e4535755dd2d5cb9b6aec48770
Nick Nazareno <35333224+nicandnaz@users.noreply.github.com>
2025-03-23 21:01:57 +0000

    Add files via upload

--- Day2LocatorPractice.java
+++ Day2LocatorPractice.java
@@ -1,6 +1,7 @@
 package seleniumIntroduction;
 
 import java.time.Duration;
+import org.openqa.selenium.*;
 
 
 import org.openqa.selenium.By;
@@ -12,6 +13,7 @@ public class Day2LocatorPractice {
 	public static void main(String[] args) {
 		testCase1();
 		testCase2();
+		testCase3();
 	}
 	
 	public static void testCase1() {
@@ -24,9 +26,17 @@ public class Day2LocatorPractice {
 		//Step 2: Input Username and Password
 		driver.findElement(By.id("inputUsername")).sendKeys("nick");
 		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("chkboxOne")).click();
+		driver.findElement(By.name("chkboxTwo")).click();
 			
-		//Step 3: Click Sigin button
-		driver.findElement(By.className("signInBtn")).click();
+		//Step 3: Click Sign-in button 
+		driver.findElement(By.className("signInBtn")).click();		
+		
+		//Verify successful sign-in 
+		boolean isSuccessfullSignin = driver.findElement(By.xpath("//p")).isDisplayed();
+		if (isSuccessfullSignin = true) {
+			System.out.println("Successfully Signed In");
+		}
 		
 		//Close browser
 		driver.quit();
@@ -46,11 +56,48 @@ public class Day2LocatorPractice {
 		//Step 3: Click Sign-in button
 		driver.findElement(By.className("signInBtn")).click();
 		
-		//Step 4: Verify error message is displayed
+		//Verify error message is displayed
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2)); //implicit wait
-		System.out.println(driver.findElement(By.cssSelector("p.error")).isDisplayed());
-		
+		boolean isErrorMessageDisplayed = driver.findElement(By.cssSelector("p.error")).isDisplayed();
+		if (isErrorMessageDisplayed = true) {
+			System.out.println("Error message is displayed!");
+		}
 		//Close browser
+		driver.quit();
+	}
+	
+	public static void testCase3() {
+		// Practice Locator using https://rahulshettyacademy.com/locatorspractice/
+		//FORGET PASSWORD
+		//Step 1: Invoke browser and go to website - https://rahulshettyacademy.com/locatorspractice/
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		
+		
+		//Step 2: Click Forgot Password using Linktext locator
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		//Step 3: Fill forgot password fields and click Reset button using Xpath locator
+		// syntax - //Tagname[@attribute='value']
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Chicken");
+		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("Egg@email.com");
+		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("1234");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//button[@class='reset-pwd-btn']")).click();
+		
+		// Verify successful forget password message
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2));
+		Boolean isResetPasswordSuccessful = driver.findElement(By.xpath("//p[@class='infoMsg']")).isDisplayed();
+		if (isResetPasswordSuccessful = true) {
+			System.out.println("Reset password is successful!");
+		}
+		
+		//close browser
 		driver.quit();
 	}
 	}
